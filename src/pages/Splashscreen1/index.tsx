a1e5ddd84e17d16a652aa0c2ed1d8ebe790febaa
jery koupun <s22129077@student.unklab.ac.id>
2025-04-27 19:12:58 +0800

    Update Splashscreen1

--- src/pages/Splashscreen1/index.tsx
+++ src/pages/Splashscreen1/index.tsx
@@ -1,12 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Logo} from '../../assets';
-import Button from '../../components/atom/Button';
 
 const Splashscreen1 = () => {
   return (
     <View style={styles.container}>
-      <Logo />
+      <Logo width={132} height={118} />
     </View>
   );
 };
