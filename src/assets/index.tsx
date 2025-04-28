1b126c430421643781b4f59195cdf74b7e11fd8c
jery koupun <s22129077@student.unklab.ac.id>
2025-04-28 17:41:37 +0800

    Adding icon-icon

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,17 @@
 import Logo from './LogoUfo.svg';
 import ArrowBack from './Vector.svg';
+import Organizationicon from './Organizationlogo.svg';
+import Announcementicon from './Announcementlogo.svg';
+import Profileicon from './profilkosong.svg';
+import Lostandfounicon from './lostandfoundlogo.svg';
+import Fullscreenicon from './fullscreenlogo.svg';
 
-export {Logo, ArrowBack};
+export {
+  Logo,
+  ArrowBack,
+  Organizationicon,
+  Announcementicon,
+  Profileicon,
+  Lostandfounicon,
+  Fullscreenicon,
+};
