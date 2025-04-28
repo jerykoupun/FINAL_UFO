13ae660b6c3f40ff7aaed74a9b01c58a0148a4cd
jery koupun <s22129077@student.unklab.ac.id>
2025-04-28 21:27:15 +0800

    Adding foto-foto

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -7,6 +7,12 @@ import Lostandfounicon from './lostandfoundlogo.svg';
 import Fullscreenicon from './fullscreenlogo.svg';
 import Image1 from './image1.svg';
 import Image2 from './uvicsimage.svg';
+import Organisasifoto from './Organizationimage.svg';
+import Galery1 from './galery1.svg';
+import Galery2 from './galery2.svg';
+import Kacamata from './kacamata.svg';
+import Tas from './tas.svg';
+import Tasbulat from './tasbulat.svg';
 
 export {
   Logo,
@@ -18,4 +24,10 @@ export {
   Fullscreenicon,
   Image1,
   Image2,
+  Organisasifoto,
+  Galery1,
+  Galery2,
+  Kacamata,
+  Tas,
+  Tasbulat,
 };
