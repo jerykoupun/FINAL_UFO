8d2f25856644aaeb53a19f04d57e12edb93b213c
fdricck <s22210431@student.unklab.ac.id>
2025-04-29 20:41:36 +0800

    Adding Profile Pages 80%

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -7,23 +7,38 @@ import TextInput from '../../components/molocules/Textinput2';
 
 const Profile = () => {
   return (
-    <View>
+    <View style={styles.page}>
       <Header title="Profile" />
-      <Profileicon width={110} height={110} />
-      <Gap height={20} />
-      <TextInput label="Name" placeholder="Enter your name" />
-      <TextInput label="Email" placeholder="Enter your email" />
-      <TextInput label="Phone Number" placeholder="Enter your phone number" />
-      <TextInput label="Address" placeholder="Enter your address" />
-      <TextInput
-        label="Password"
-        placeholder="Enter your password"
-        secureTextEntry={true}
-      />
+      <View style={styles.container}>
+        <View style={styles.avatarWrapper}>
+          <Profileicon width={110} height={110} />
+        </View>
+        <Gap height={20} />
+        <TextInput label="Name" placeholder="Enter your name" />
+        <TextInput label="Email" placeholder="Enter your email" />
+        <TextInput label="Phone Number" placeholder="Enter your phone number" />
+        <TextInput label="Address" placeholder="Enter your address" />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          secureTextEntry={true}
+        />
+      </View>
     </View>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+});
