fdf1ee8c8b42431c30847e3239c0660988418cd2
jerykoupun <s22129077@student.unklab.ac.id>
2025-04-29 15:27:56 +0800

    Adding Profile Pages 65%

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -8,9 +8,12 @@ import Gap from '../../components/atom/Gap';
 const Profile = () => {
   return (
     <View style={styles.page}>
-      <Header title="Profile" />
+      <View style={styles.headerContainer}>
+        <Header title="Profile" />
+      </View>
       <View style={styles.container}>
         <Profileicon width={110} height={110} />
+        <Gap height={30} />
       </View>
       <TextInput placeholder={'Enter your Username'} label={'Username'} />
       <Gap height={20} />
@@ -32,6 +35,11 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  headerContainer: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 20,
   },
   container: {
     paddingHorizontal: 38,
