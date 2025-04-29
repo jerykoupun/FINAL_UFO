b2e7dbdea3aa490ea698b326b3d42284cbb3a1b3
fdricck <s22210431@student.unklab.ac.id>
2025-04-29 20:32:23 +0800

    Adding Profile Pages 50%

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,29 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Gap from '../../components/atom/Gap';
+import {Profileicon} from '../../assets';
+import Header from '../../components/molocules/Header';
+import TextInput from '../../components/molocules/Textinput2';
 
-const index = () => {
+const Profile = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Header title="Profile" />
+      <Profileicon width={110} height={110} />
+      <Gap height={20} />
+      <TextInput label="Name" placeholder="Enter your name" />
+      <TextInput label="Email" placeholder="Enter your email" />
+      <TextInput label="Phone Number" placeholder="Enter your phone number" />
+      <TextInput label="Address" placeholder="Enter your address" />
+      <TextInput
+        label="Password"
+        placeholder="Enter your password"
+        secureTextEntry={true}
+      />
     </View>
   );
 };
 
-export default index;
+export default Profile;
 
 const styles = StyleSheet.create({});
